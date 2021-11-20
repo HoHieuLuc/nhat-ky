da4de0065212e946ecf8cd9d16d4b1801e4deb13
Ho Hieu Luc <kingofika123@gmail.com>
2021-11-20 16:29:35 +0700

    thêm xóa nhật ký

--- public/nhatky-browser.js
+++ public/nhatky-browser.js
@@ -1,5 +1,9 @@
 const nhatkysDOM = document.querySelector('.nhatkys');
 const loadingDOM = document.querySelector('.loading-text');
+const formDOM = document.querySelector('.nhatky-form');
+const formAlertDOM = document.querySelector('.form-alert');
+const tieuDeDOM = document.querySelector('#tieu_de');
+const noiDungDOM = document.querySelector('#noi_dung');
 
 // hiển thị nhật ký
 const showNhatKys = async () => {
@@ -21,12 +25,16 @@ const showNhatKys = async () => {
                 <div>
                     <div class="d-flex justify-content-between">
                         <p class="text-muted">Ngày đăng: ${ngay_dang}</p>
-                        ${lan_sua_cuoi !== null ? 
-                            `<p class="text-muted">Lần sửa cuối: ${lan_sua_cuoi}</p>` :
-                            ``
-                        }
+                        ${lan_sua_cuoi !== null ?
+                    `<p class="text-muted">Lần sửa cuối: ${lan_sua_cuoi}</p>` :
+                    ``
+                }
                     </div>
                     <p>${noi_dung}</p>
+                </div>
+                <div class="d-flex justify-content-end gap-2">
+                    <a class="btn btn-link" href="nhatky.html?id=${nhatKyID}">Sửa</a>
+                    <button type="button" class="btn btn-link delete-nhatky" data-id="${nhatKyID}">Xóa</button>
                 </div>
             </div>
             `;
@@ -39,3 +47,46 @@ const showNhatKys = async () => {
 }
 
 showNhatKys();
+
+// xóa
+nhatkysDOM.addEventListener('click', async (e) => {
+    const el = e.target;
+    if (el.classList.contains('delete-nhatky')){
+        //console.log(el.dataset.id);
+        loadingDOM.style.visibility = 'visible';
+        const id = el.dataset.id;
+        try {
+            await axios.delete(`/api/v1/nhatky/${id}`);
+            showNhatKys();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    loadingDOM.style.visibility = 'hidden';
+});
+
+// form thêm
+
+formDOM.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const tieu_de = tieuDeDOM.value;
+    const noi_dung = noiDungDOM.value;
+    //alert(tieu_de + ', ' + noi_dung);
+    try {
+        await axios.post('/api/v1/nhatky', { tieu_de, noi_dung });
+        showNhatKys();
+        tieuDeDOM.value = '';
+        noiDungDOM.value = '';
+        formAlertDOM.style.display = 'block';
+        formAlertDOM.textContent = `Thêm nhật ký thành công`;
+        formAlertDOM.classList.add('text-success');
+    } catch (error) {
+        formAlertDOM.classList.add('text-danger');
+        formAlertDOM.style.display = 'block';
+        formAlertDOM.innerHTML = `Có lỗi xảy ra`;
+    }
+    setTimeout(() => {
+        formAlertDOM.style.display = 'none';
+        formAlertDOM.classList.remove('text-success', 'text-danger');
+    }, 3000);
+});
